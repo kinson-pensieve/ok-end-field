988a343617bf8e58a2b90d31cf548b40e67adc3a
AliceJump <2630923991@qq.com>
2026-02-27 23:45:41 +0800

    更新label资源

--- x-anylabeling-asset/compress.py
+++ x-anylabeling-asset/compress.py
@@ -2,4 +2,4 @@ from ok.feature.FeatureSet import compress_copy_x_anylabeling
 
 # from ok.util.coco import compress_and_copy_coco
 
-compress_copy_x_anylabeling('x-anylabeling-asset/project_dir', 'assets')
+compress_copy_x_anylabeling('x-anylabeling-asset/project_dir', 'assets',generate_label_enmu="src.data.FeatureList")
